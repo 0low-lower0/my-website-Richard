05459161f49dbecf7915316ad994caebd4e8ae0c
0low-lower0 <naipeter06@gmail.com>
2026-04-26 19:19:17 -0400

    Add files via upload

---
+++ js/scripst.js
@@ -0,0 +1,13 @@
+function videoFunction(){
+            document.getElementById("myComent").innerHTML += "A good game video";
+ }
+
+ function emailMessage(){
+    document.getElementById("emailBox"),innerHTML += "Please enter a valid email adddress";
+ }
+
+ function blankMessage(){
+    document.getElementById("emailBox").innerHTML = "Email:";
+ }
+
+ 
